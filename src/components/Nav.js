92dde44bdd34052d79a01cd3d4abff48e6d2058f
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-04 15:45:58 +0000

    add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+                <li>D</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
