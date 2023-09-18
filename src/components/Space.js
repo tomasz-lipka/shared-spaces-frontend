7c9fb663a7946bc278612a61a0b9b4b3a75b596b
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-18 14:06:58 +0000

    fetch space

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,15 +1,36 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import config from '../config';
 
 function Space() {
     const { id } = useParams();
     const navigate = useNavigate()
-    
+    const [space, setSpace] = useState('');
+
+    const fetchSpace = async () => {
+        const response = await fetch(config.apiUrl + '/spaces/' + id, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${sessionStorage.getItem("access_token")}`
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            setSpace(data);
+        }
+    };
+
+    useEffect(() => {
+        fetchSpace();
+    });
 
     return (
         <div>
-            <button onClick={() => navigate(-1)}>Go back</button>
-            <div>
-                Space: {id}
+            <div className="left-div">
+                <button onClick={() => navigate(-1)}>Go back</button>
+            </div>
+            <div className="right-div">
+                <h3>Space {space.name}</h3>
             </div>
         </div>
     );
