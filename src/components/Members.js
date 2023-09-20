262a3d7bdda6673013dfb8884e604dfa76d150ba
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-20 09:15:18 +0000

    fetch members

--- src/components/Members.js
+++ src/components/Members.js
@@ -6,6 +6,8 @@ function Members() {
 
     const { id } = useParams();
     const [space, setSpace] = useState('');
+    const [members, setMembers] = useState([]);
+    const [loadingMembers, setLoadingMembers] = useState(false);
     const navigate = useNavigate()
 
     const fetchSpace = async () => {
@@ -21,20 +23,47 @@ function Members() {
         }
     };
 
+    const fetchMembers = async () => {
+        setLoadingMembers(true)
+        const response = await fetch(config.apiUrl + '/spaces/' + id + '/members', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${sessionStorage.getItem("access_token")}`
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            setMembers(data);
+            setLoadingMembers(false)
+        }
+    };
+
     useEffect(() => {
         fetchSpace();
+        fetchMembers();
     }, []);
 
     return (
         <div>
             <div className="left-div">
-                <br/>
+                <br />
                 <a href="#" onClick={(e) => { e.preventDefault(); navigate(-1); }}>{'<<'}Go back</a>
-                
+
 
             </div>
             <div className="right-div">
                 <h3>{space.name} {'>'} members</h3>
+                <p>{loadingMembers ? 'Loading...' : '\u00A0'}</p>
+                {members.map((value) => {
+                            console.log(value)
+
+                    return (
+                        <div className="member" key={value.user.id}>
+                            <p>{value.user.login}</p>
+                            <p>{value.is_admin ? 'is admin' : 'no admin'}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div >
     );
