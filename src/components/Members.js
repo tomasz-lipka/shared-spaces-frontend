4ca5571cbc82a2d5feb35abc8f9ec3010de6316c
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-20 10:11:46 +0000

    add member

--- src/components/Members.js
+++ src/components/Members.js
@@ -8,7 +8,11 @@ function Members() {
     const [space, setSpace] = useState('');
     const [members, setMembers] = useState([]);
     const [loadingMembers, setLoadingMembers] = useState(false);
+    const [loadingAddMember, setLoadingAddMember] = useState(false);
+    const [userId, setUserId] = useState('');
     const navigate = useNavigate()
+    const [msg, setMsg] = useState('');
+    const [isAdmin, setIsAdmin] = useState(false);
 
     const fetchSpace = async () => {
         const response = await fetch(config.apiUrl + '/spaces/' + id, {
@@ -38,9 +42,40 @@ function Members() {
         }
     };
 
+    const addMember = async () => {
+        setLoadingAddMember(true)
+        const response = await fetch(config.apiUrl + '/spaces/' + id + '/members', {
+            method: 'POST',
+            body: `{
+            "user-id": "${userId}"
+          }`,
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem("access_token")}`
+            },
+        });
+        if (response.ok) {
+            fetchMembers();
+            setLoadingAddMember(false);
+        } else {
+            const errorMessage = await response.text();
+            setMsg(errorMessage);
+            setLoadingAddMember(false);
+        }
+    };
+
+    const setCurrentUserAdminStatus = () => {
+        members.forEach((value) => {
+            if (value.user.login === sessionStorage.getItem("currentUser")) {
+                setIsAdmin(true);
+            }
+        });
+    };
+
     useEffect(() => {
         fetchSpace();
         fetchMembers();
+        setCurrentUserAdminStatus()
     }, []);
 
     return (
@@ -48,14 +83,27 @@ function Members() {
             <div className="left-div">
                 <br />
                 <a href="#" onClick={(e) => { e.preventDefault(); navigate(-1); }}>{'<<'}Go back</a>
-
+                <hr />
+                <div>
+                    <h4>Add member</h4>
+                    <input
+                        placeholder="Login of member here..."
+                        type="text"
+                        value={userId}
+                        onChange={(e) => setUserId(e.target.value)}
+                    />
+                </div>
+                <button onClick={addMember} disabled={loadingAddMember || !isAdmin}>
+                    {loadingAddMember ? 'Adding...' : 'Add'}
+                </button>
+                <p>{msg}</p>
+                <hr />
 
             </div>
             <div className="right-div">
                 <h3>{space.name} {'>'} members</h3>
                 <p>{loadingMembers ? 'Loading...' : '\u00A0'}</p>
                 {members.map((value) => {
-                            console.log(value)
 
                     return (
                         <div className="member" key={value.user.id}>
