e373db2772438dcdaf1531704c825e06e5a70465
Tomasz Lipka <lipkatk@gmail.com>
2023-09-26 13:03:32 +0200

    update image

--- src/components/EditShare.js
+++ src/components/EditShare.js
@@ -6,12 +6,15 @@ import { makeShareRequest } from "../Helper"
 function EditShare({ originalText, shareId, setMsg, setEdit, fetchShares }) {
 
     const [text, setText] = useState(originalText);
+    const [file, setFile] = useState(null);
+
 
     async function updateShare() {
         setMsg(Config.waitMsg)
         let bodyContent = new FormData()
         bodyContent.append("text", text)
-        // bodyContent.append("file", "/workspaces/shared-spaces/project/test/test-image.jpg");
+        if (file) { bodyContent.append("file", file) }
+        bodyContent.append("file", file);
         let response = await makeShareRequest('/shares/' + shareId, 'PUT', bodyContent)
         if (response.ok) {
             fetchShares()
@@ -23,21 +26,35 @@ function EditShare({ originalText, shareId, setMsg, setEdit, fetchShares }) {
 
     function closeEditor() { setEdit(false) }
 
+    function handleFileChange(event) {
+        setFile(event.target.files[0]);
+    };
+
     return (
         <div>
-            <textarea
-                type="text"
-                placeholder="Enter your text here..."
-                rows="5"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-            />
-            <br />
-            <button onClick={updateShare} >Update</button>
-            <button onClick={closeEditor} >Cancel</button>
+            <div className='div-flex-basic'>
+                <div className='div-flex'>
+                    <textarea
+                        type="text"
+                        placeholder="Enter your text here..."
+                        rows="5"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    />
+                </div>
+                <div className='div-flex'>
+                    <input
+                        type="file"
+                        accept=".jpg, .jpeg, .png"
+                        onChange={handleFileChange}
+                    />
+                    <button onClick={updateShare} >Update</button>
+                    <br />
+                    <button onClick={closeEditor} >Cancel</button>
+                </div>
+            </div>
         </div>
     );
-
 }
 
 export default EditShare
