cea7f7707edeb76ec29f98845bd3bfaa4cbfa8f6
Tomasz Lipka <lipkatk@gmail.com>
2023-09-26 10:15:56 +0200

    refactor Shares

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,18 +1,17 @@
+import React from 'react';
 import { format } from "date-fns";
 import { makeRequest } from "../Helper"
-import React, { useState } from 'react';
+import Config from '../Config';
 
 function Share({ share, fetchShares, setMsg }) {
 
     const formattedTimestamp = format(new Date(share.timestamp), "dd/MM/yyyy HH:mm");
     const showButton = sessionStorage.getItem("currentUser") === share.user.login;
-    const [loading, setLoading] = useState(false);
 
     const deleteShare = async (shareId) => {
-        setMsg('Please wait...')
-        setLoading(true)
-        await makeRequest('/shares/' + shareId, 'DELETE', null)
-        fetchShares();
+        setMsg(Config.waitMsg)
+        let response = await makeRequest('/shares/' + shareId, 'DELETE', null)
+        response.ok ? fetchShares() : setMsg(await response.text())
     };
 
     return (
@@ -24,7 +23,7 @@ function Share({ share, fetchShares, setMsg }) {
             <div className="right-div-flex">
                 {showButton && (
                     <button
-                        onClick={() => deleteShare(share.id)} disabled={loading}>Delete</button>
+                        onClick={() => deleteShare(share.id)}>Delete</button>
                 )}
                 <p>{share.text}</p>
             </div>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const Config = {
     apiUrl: 'http://35.175.215.26:8000',
+    waitMsg: 'Please wait...',
+    blankMsg: '\u00A0'
 };
 
 export default Config;
