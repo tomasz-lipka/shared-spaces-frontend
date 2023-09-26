2cd0f2dda171ec6bad5c3f35ae558a39c4f747b8
Tomasz Lipka <lipkatk@gmail.com>
2023-09-26 10:23:19 +0200

    add edit button

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { format } from "date-fns";
 import { makeRequest } from "../Helper"
 import Config from '../Config';
@@ -6,13 +6,17 @@ import Config from '../Config';
 function Share({ share, fetchShares, setMsg }) {
 
     const formattedTimestamp = format(new Date(share.timestamp), "dd/MM/yyyy HH:mm");
-    const showButton = sessionStorage.getItem("currentUser") === share.user.login;
+    const showButtons = sessionStorage.getItem("currentUser") === share.user.login;
 
-    const deleteShare = async (shareId) => {
+    async function deleteShare(shareId) {
         setMsg(Config.waitMsg)
         let response = await makeRequest('/shares/' + shareId, 'DELETE', null)
         response.ok ? fetchShares() : setMsg(await response.text())
     };
+
+    async function editShare(shareId) {
+
+    }
 
     return (
         <div className="share" key={share.id}>
@@ -21,10 +25,12 @@ function Share({ share, fetchShares, setMsg }) {
                 <p>{formattedTimestamp}</p>
             </div>
             <div className="right-div-flex">
-                {showButton && (
-                    <button
-                        onClick={() => deleteShare(share.id)}>Delete</button>
-                )}
+                {showButtons ? (
+                    <div>
+                        <button onClick={() => editShare(share.id)}>Edit</button>
+                        <button onClick={() => deleteShare(share.id)}>Delete</button>
+                    </div>
+                ) : null}
                 <p>{share.text}</p>
             </div>
         </div>
