2c44ac2fa2bba074f700f66bf5725e3d4fc70662
Tomasz Lipka <lipkatk@gmail.com>
2023-09-27 11:44:25 +0200

    add Register component

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,90 +1,24 @@
 import React, { useState } from 'react';
 import Config from '../../Config';
 import Login from './Login';
+import Register from './Register';
 
 function Auth({ setAuthenticated }) {
 
-  const [loading, setLoading] = useState(false);
-  const [msg, setMsg] = useState('');
-
-  const [registerLogin, setRegisterLogin] = useState('');
-  const [registerPwd, setRegisterPwd] = useState('');
-  const [confirmPwd, setConfirmPwd] = useState('');
-  const [registerMsg, setRegisterMsg] = useState('');
-
-
-  async function handleRegister() {
-    setMsg('')
-    setRegisterMsg('')
-    const response = await fetch(Config.apiUrl + '/register', {
-      method: 'POST',
-      body: `{
-        "login": "${registerLogin}",
-        "password": "${registerPwd}",
-        "confirm-password": "${confirmPwd}"
-      }`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      const msg = await response.text();
-      setRegisterMsg(msg)
-    } else {
-      const errorMessage = await response.text();
-      setRegisterMsg(errorMessage);
-    }
-
-
-
-  };
+  const [msg, setMsg] = useState(Config.blankMsg);
 
   return (
     <div className="auth-page">
       <span className='auth-logo-text'>Shared Spaces</span>
-      <p>{msg}</p>
-      <br></br>
-
+      <p className='auth-page-msg'>{msg}</p>
       <Login setAuthenticated={setAuthenticated} setMsg={setMsg} />
-
-      <br></br>
-      <h3>or</h3>
-      <br></br>
-
-      <div>
-        <label>Login: </label>
-        <input
-          className='auth-input'
-          type="text"
-          value={registerLogin}
-          onChange={(e) => setRegisterLogin(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          className='auth-input'
-          type="password"
-          value={registerPwd}
-          onChange={(e) => setRegisterPwd(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Confirm password: </label>
-        <input
-          className='auth-input'
-          type="password"
-          value={confirmPwd}
-          onChange={(e) => setConfirmPwd(e.target.value)}
-        />
-      </div>
-      <button onClick={handleRegister}>Register</button>
-      <p>{registerMsg}</p>
-
-      <p>
-        This is an educational project. Please don't put any confidential data.
-        Especially password that you already use somewhere else, personal information, photos and
-        other data that you don't want to loose.
+      <br /><hr /><br />
+      <Register setMsg={setMsg} />
+      <br />
+      <p class='disclaimer'>
+        This is an educational project. Please don't put any confidential data in.<br />
+        Especially passwords that you already use somewhere else, personal information,<br />
+        photos and data that you don't want to lose.
       </p>
     </div >
   );
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,27 +2,22 @@ import React, { useState } from 'react';
 import { makeRequest } from "../../Helper"
 import Config from '../../Config';
 
-function Login({ setAuthenticated, setMsg }) {
-    const [loading, setLoading] = useState(false);
+function Register({ setMsg }) {
     const [login, setLogin] = useState('');
     const [pwd, setPwd] = useState('');
+    const [confirmPwd, setConfirmPwd] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    async function handleLogin() {
+    async function handleRegister() {
         setMsg(Config.blankMsg)
-        setLoading(true);
+        setLoading(true)
         let requestBody = JSON.stringify({
             "login": login,
-            "password": pwd
+            "password": pwd,
+            "confirm-password": confirmPwd
         });
-        let response = await makeRequest('/login', 'POST', requestBody);
-        if (response.ok) {
-            const data = await response.json();
-            sessionStorage.setItem('access_token', data.access_token);
-            sessionStorage.setItem('currentUser', login);
-            setAuthenticated(true);
-        } else {
-            setMsg(await response.text())
-        }
+        let response = await makeRequest('/register', 'POST', requestBody);
+        setMsg(await response.text())
         setLoading(false)
     };
 
@@ -38,7 +33,7 @@ function Login({ setAuthenticated, setMsg }) {
                     onChange={(e) => setLogin(e.target.value)}
                 />
             </div>
-            <div >
+            <div>
                 <label>Password: </label>
                 <br />
                 <input
@@ -48,11 +43,21 @@ function Login({ setAuthenticated, setMsg }) {
                     onChange={(e) => setPwd(e.target.value)}
                 />
             </div>
-            <button onClick={handleLogin} disabled={loading}>
-                {loading ? 'Logging in...' : 'Login'}
+            <div>
+                <label>Confirm password: </label>
+                <br />
+                <input
+                    className='auth-input'
+                    type="password"
+                    value={confirmPwd}
+                    onChange={(e) => setConfirmPwd(e.target.value)}
+                />
+            </div>
+            <button onClick={handleRegister}>
+                {loading ? 'Pending...' : 'Register'}
             </button>
         </div>
     );
 }
 
-export default Login
+export default Register
