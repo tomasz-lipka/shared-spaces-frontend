7a5f96921dca925b7633bb47f0ab1e98986ba3e6
Tomasz Lipka <lipkatk@gmail.com>
2023-10-06 13:20:47 +0200

    temp fix of image loading bug

--- src/components/main/share/CreateShare.js
+++ src/components/main/share/CreateShare.js
@@ -20,6 +20,12 @@ function CreateShare({ setMsg, spaceId, fetchShares }) {
         bodyContent.append("text", text);
         if (file) { bodyContent.append("file", file) }
         let response = await makeShareRequest('/spaces/' + spaceId + '/shares', 'POST', bodyContent)
+
+        // TODO is this good?
+        setTimeout(() => {
+        }, 5000);
+        // end TODO
+
         if (response.ok) {
             fetchShares()
             setFile(null)
