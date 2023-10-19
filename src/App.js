f7968d3e14c19f30c5603f5e9eb479136a98293c
Tomasz Lipka <lipkatk@gmail.com>
2023-10-19 09:26:43 +0200

    add import React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Auth from './components/auth/Auth';
 import MainMenu from './components/main/MainMenu';
